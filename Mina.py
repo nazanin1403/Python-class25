814c6bd7a76f2df9054ef73c52b95d2a7dd0cad7
nazanin1403 <nazaninzararavanraz@gmail.com>
2024-07-25 10:54:03 +0330

    Add files via upload

---
+++ Mina.py
@@ -0,0 +1,5 @@
+x=int(input("Enter the number"))
+if x<10:
+    print('pass')
+else:
+    print('faild')
---
+++ Nazanin.py
@@ -0,0 +1,6 @@
+score=10
+if score<10:
+    print('pass')
+if score>10:
+    else:
+        print('faild')
